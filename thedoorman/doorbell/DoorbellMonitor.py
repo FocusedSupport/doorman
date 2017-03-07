91ed6b6e2f50cab2ea702d185b907f8b7389e9e6
Scott Lipcon <scott.lipcon@focusedsupport.com>
2017-03-07 16:57:33 -0500

    WIP #2 - temporary post to slack

--- thedoorman/doorbell/DoorbellMonitor.py
+++ thedoorman/doorbell/DoorbellMonitor.py
@@ -1,12 +1,18 @@
 import _thread
 import time
 import RPi.GPIO as GPIO
+import json
+import requests
+
 
 class DoorbellMonitor(object):
 
     lastTime = 0
     ignoreTimeSeconds = 5.0
     BUTTON_CHANNEL = 17
+
+    # this one posts to #doormantest for now
+    webhook_url = 'https://hooks.slack.com/services/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'
 
     def __init__(self):
         print("I'm like a constructor!")
@@ -18,6 +24,17 @@ class DoorbellMonitor(object):
         except _thread.error:
             print("Error starting Doorbell Monitor thread")
 
+    def temporaryNotify(self):
+        formattedTime = time.strftime('%l:%M%p %Z on %b %d, %Y')
+        slack_data = {"username": "doorman", "text": ":door: Someone is ringing the doorbell at " + formattedTime}
+
+        response = requests.post(
+            self.webhook_url, data=json.dumps(slack_data),
+            headers={'Content-Type': 'application/json'}
+        )
+        if response.status_code != 200:
+            print("Error posting to slack")
+
     def _monitor(self):
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.BUTTON_CHANNEL, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -27,5 +44,6 @@ class DoorbellMonitor(object):
             currentTime = time.time()
             if currentTime - self.lastTime > self.ignoreTimeSeconds:
                 print('Button Pressed callback')
+                self.temporaryNotify()
                 self.lastTime = currentTime
 
