5888a07ce077e4f23d0026ca59e80839db96e339
Scott Lipcon <scott.lipcon@focusedsupport.com>
2017-03-07 16:31:15 -0500

    WIP #2 : attempt to use interrupt for button

--- thedoorman/doorbell/DoorbellMonitor.py
+++ thedoorman/doorbell/DoorbellMonitor.py
@@ -4,6 +4,10 @@ import RPi.GPIO as GPIO
 
 class DoorbellMonitor(object):
 
+    lastTime = 0
+    ignoreTimeSeconds = 5.0
+    BUTTON_CHANNEL = 17
+
     def __init__(self):
         print("I'm like a constructor!")
         self._count = 0
@@ -15,12 +19,13 @@ class DoorbellMonitor(object):
             print("Error starting Doorbell Monitor thread")
 
     def _monitor(self):
-
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.BUTTON_CHANNEL, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
         while True:
-            input_state = GPIO.input(17)
-            if input_state == False:
-                print('Button Pressed')
-                time.sleep(0.2)
+            channel = GPIO.wait_for_edge( self.BUTTON_CHANNEL, GPIO.RISING )
+            currentTime = time.time()
+            if currentTime - self.lastTime > self.ignoreTimeSeconds:
+                print('Button Pressed callback')
+                self.lastTime = currentTime
+
