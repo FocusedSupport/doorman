a21b83a9747d7060fda47702009e1d3780f3470d
Scott Lipcon <scott.lipcon@focusedsupport.com>
2017-04-13 17:14:46 -0400

    Resolves #10 - triggers both relays

--- thedoorman/components/devices/lock.py
+++ thedoorman/components/devices/lock.py
@@ -4,17 +4,46 @@ from pydispatch import dispatcher
 
 from ..dispatcher.signals import Signals, Senders
 from ..slack.user_manager import UserManager
-
+import RPi.GPIO as GPIO
 
 class Lock(object):
 
     def __init__(self):
+        self.MAIN_DOOR_RELAY_CHANNEL = 27
+        self.SIDE_DOOR_RELAY_CHANNEL = 22
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.MAIN_DOOR_RELAY_CHANNEL, GPIO.OUT)
+        GPIO.setup(self.SIDE_DOOR_RELAY_CHANNEL, GPIO.OUT)
+
+        self._lock(self.MAIN_DOOR_RELAY_CHANNEL)
+        self._lock(self.SIDE_DOOR_RELAY_CHANNEL)
+
+
         dispatcher.connect(self._handle_message, signal=Signals.UNLOCK, sender=Senders.SLACKBOT)
         self._run()
 
     def _handle_message(self, door=None, duration=None, userid=None):
         username = UserManager().get_username(userid)
-        print("unlock: " + door + " for " + duration + " seconds, initiated by "+username)
+
+        if door.lower() == "main":
+            button = self.MAIN_DOOR_RELAY_CHANNEL;
+        elif door.lower() == "side":
+            button = self.SIDE_DOOR_RELAY_CHANNEL;
+        else:
+            print("Unknown door " + door + ", not unlocking")
+            return
+
+        print("unlocking " + door + " for " + duration + " seconds, initiated by "+username)
+        self._unlock(button)
+        time.sleep(float(duration))
+        self._lock(button)
+
+    def _lock(self, door):
+        GPIO.output(door, GPIO.HIGH)
+
+    def _unlock(self, door):
+        GPIO.output(door, GPIO.LOW)
 
     def _run(self):
         while True:
