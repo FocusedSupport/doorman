d0b0a6f7382877589cb5d8eb9fe3d222acfd8ae7
Scott Lipcon <scott.lipcon@focusedsupport.com>
2017-05-10 10:12:09 -0400

    Fixes #17 - detect short spikes/drops
    prevents accidental triggering of doorbell

--- thedoorman/components/devices/doorbell_monitor.py
+++ thedoorman/components/devices/doorbell_monitor.py
@@ -2,6 +2,7 @@ import time
 
 import RPi.GPIO as GPIO
 
+from time import sleep
 from pydispatch import dispatcher
 from ..dispatcher.signals import Signals, Senders
 from .gpio import Pins
@@ -12,6 +13,7 @@ class DoorbellMonitor(object):
     def __init__(self):
         self.lastTime = 0
         self.ignoreTimeSeconds = 5.0
+        self.bounce = 0.050   # in seconds
         dispatcher.connect(self._handle_unlockevent, signal=Signals.UNLOCKED, sender=dispatcher.Any)
         dispatcher.connect(self._handle_lockevent, signal=Signals.LOCKED, sender=dispatcher.Any)
         self._run()
@@ -29,11 +31,19 @@ class DoorbellMonitor(object):
         GPIO.setup(Pins.BUTTON_CHANNEL, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
         while True:
-            GPIO.wait_for_edge(Pins.BUTTON_CHANNEL, GPIO.FALLING)
-            currentTime = time.time()
-            if currentTime - self.lastTime > self.ignoreTimeSeconds:
-                self.lastTime = currentTime
-                self._notify()
+            GPIO.wait_for_edge(Pins.BUTTON_CHANNEL, GPIO.RISING)
+            pinValue = GPIO.input(Pins.BUTTON_CHANNEL)
+
+            print("input after rising edge at " + str(time.time()))
+            sleep(self.bounce)
+            pinValue = GPIO.input(Pins.BUTTON_CHANNEL)
+            if pinValue == 0:
+                print("skipping bouncing ring")
+            else:
+                currentTime = time.time()
+                if currentTime - self.lastTime > self.ignoreTimeSeconds:
+                    self.lastTime = currentTime
+                    self._notify()
 
     def _notify(self):
         dispatcher.send(signal=Signals.DOORBELL, sender=self)
